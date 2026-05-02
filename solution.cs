e808840f3dbee8959c06e687a047e8bbe9ba8c59
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-05-02 22:16:10 +0000

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int ArrayPairSum(int[] nums) {
+        Array.Sort(nums);
+        int sum = 0;
+        for(int i = 0; i < nums.Length; i += 2){
+            sum += Math.Min(nums[i], nums[i + 1]);
+        }
+
+        return(sum);
+    }
+}
